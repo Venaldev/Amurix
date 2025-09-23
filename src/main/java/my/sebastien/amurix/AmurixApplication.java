7e5daf2584b2a7901c134bd74f61f2b0b0011389
Venal <venaldev@gmail.com>
2025-09-23 16:57:22 -0400

    Push local repo

---
+++ src/main/java/my/sebastien/amurix/AmurixApplication.java
@@ -0,0 +1,13 @@
+package my.sebastien.amurix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmurixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmurixApplication.class, args);
+	}
+
+}
---
+++ src/test/java/my/sebastien/amurix/AmurixApplicationTests.java
@@ -0,0 +1,13 @@
+package my.sebastien.amurix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmurixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
