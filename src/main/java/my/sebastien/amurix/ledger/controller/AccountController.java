8b7d0973de8b0f47f6a044fe1e63c3446bd5fd5e
Venal <venaldev@gmail.com>
2025-10-12 21:40:07 -0400

    Fix mapping name in AccountController

--- src/main/java/my/sebastien/amurix/ledger/controller/AccountController.java
+++ src/main/java/my/sebastien/amurix/ledger/controller/AccountController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping(name = "accounts")
+@RequestMapping("/accounts")
 @RequiredArgsConstructor
 public class AccountController {
 
